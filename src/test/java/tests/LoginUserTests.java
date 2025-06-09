f03682feaac05f6e4962eee82b55bf52db2e0475
Stefan Arsenie <arseniestefan@gmail.com>
2025-06-09 17:42:11 +0300

    add description

--- src/test/java/tests/LoginUserTests.java
+++ src/test/java/tests/LoginUserTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import api_pom.login_user.LoginUserResponseDto;
+import io.qameta.allure.Description;
 import io.restassured.response.Response;
 import lombok.extern.log4j.Log4j2;
 import org.testng.Assert;
@@ -17,6 +18,7 @@ public class LoginUserTests {
     public void setUp() {
         loginUserRequest = new LoginUserRequest();
     }
+    @Description("Verify login with valid credentials")
     @Test
     public void loginUserWithCorrectCredentials() {
         loginUserResponseDto = loginUserRequest.login(Constants.USER_NAME, Constants.PASSWORD);
@@ -24,6 +26,7 @@ public class LoginUserTests {
         Assert.assertEquals(response.getStatusCode(), 200);
         Assert.assertFalse(loginUserResponseDto.getToken().isEmpty(), "Token should not be empty");
     }
+    @Description("Verify login with invalid credentials")
     @Test
     public void loginUserWithWrongAdminName() {
         loginUserResponseDto = loginUserRequest.login("addmin", Constants.PASSWORD);
